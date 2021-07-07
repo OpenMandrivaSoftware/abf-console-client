48f66a4c6792104454d4ebe46d6680931f9b7d07
Alexander Stefanov <alexander@mezon.ru>
2021-07-07 08:30:51 +0000

    rosa now is 2021.1

--- abf.py
+++ abf.py
@@ -1400,7 +1400,7 @@ def build(return_ids=False):
         if str(build_repositories[0]).find("rosa2019.0") >= 0 or str(build_repositories[0]).find("rosa2016.1") >= 0:
             try_arches = ['i586','x86_64']
 
-        if str(build_repositories[0]).find("rosa2019.1") >= 0:
+        if str(build_repositories[0]).find("rosa2021.1") >= 0:
             try_arches = ['i686','x86_64','aarch64']
 
         if str(build_repositories[0]).find("openmandriva") >= 0 or str(build_repositories[0]).find("4.2") >= 0:
